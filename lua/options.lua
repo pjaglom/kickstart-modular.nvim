0a43e32a3ac62c4dea150bd004f2e4ab44584936
Peter Jaglom <pjaglom@gmail.com>
2024-01-02 22:14:05 -0800

    turned off vim-sleuth and disabled syncing with OS clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -18,7 +18,7 @@ vim.o.mouse = 'a'
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+-- vim.o.clipboard = 'unnamedplus'
 
 -- Set shiftwidth to 4 spaces
 vim.o.shiftwidth = 4
