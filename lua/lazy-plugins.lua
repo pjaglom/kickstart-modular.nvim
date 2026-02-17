75e023566f8be15e9598efb2a916a6dce77915ef
Peter Jaglom <peter@peterjaglom.com>
2026-02-17 12:37:31 -0800

    turned mouse back on in hardtime.nvim

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -56,7 +56,9 @@ require('lazy').setup({
   {
     'm4xshen/hardtime.nvim',
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-    opts = {},
+    opts = {
+      disable_mouse = false,
+    },
   },
 
   -- snacks.nvim
